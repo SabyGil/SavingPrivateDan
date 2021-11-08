73049928d95f5def6e9a83f970d83acc56236827
SabyGil <sabygil@yahoo.com>
2021-11-08 17:03:52 -0500

    commit after first video

---
+++ src/com/zerulus/game/GameLauncher.java
@@ -0,0 +1,13 @@
+package com.zerulus.game;
+
+public class GameLauncher {
+    public GameLauncher(){
+        new Window();
+    }
+
+
+    public static void main(String[] args){
+//        System.out.println("hello world");
+        new GameLauncher();
+    }
+}
